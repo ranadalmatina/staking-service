98cc0e8081a599ee2de2a88916f709507b9b18f7
Dalmatina <97528934+ranadalmatina@users.noreply.github.com>
2022-05-12 18:29:26 +0100

    Add serialization to the data structures.

--- ss/avalanche/datastructures.py
+++ ss/avalanche/datastructures.py
@@ -1,3 +1,10 @@
+"""
+Data structures required when creating transactions on Avalanche. Designed from
+the specifications located at
+https://docs.avax.network/specs/coreth-atomic-transaction-serialization
+"""
+
+from hexbytes import HexBytes
 from .tools import num_to_int
 
 class EVMOutput:
@@ -9,6 +16,12 @@ class EVMOutput:
         assert len(self.amount) == 8
         assert len(self.asset_id) == 32
 
+    def to_bytes(self, hex=False):
+        serialized = self.address + self.amount + self.asset_id
+        if hex:
+            return HexBytes(serialized).hex()
+        return serialized
+
 
 class EVMInput(EVMOutput):
     def __init__(self, address: bytes, amount: bytes, asset_id: bytes, nonce: bytes):
@@ -16,10 +29,16 @@ class EVMInput(EVMOutput):
         self.nonce = nonce
         assert len(self.nonce) == 8
 
+    def to_bytes(self, hex=False):
+        serialized = self.address + self.amount + self.asset_id + self.nonce
+        if hex:
+            return HexBytes(serialized).hex()
+        return serialized
+
 
 class SECPTransferOutput:
-    def __init__(self, type_id: bytes, amount: bytes, locktime: bytes, threshold: bytes, addresses: list[bytes]):
-        self.type_id = type_id
+    def __init__(self, amount: bytes, locktime: bytes, threshold: bytes, addresses: list[bytes]):
+        self.type_id = num_to_int(0x00000007)
         self.amount = amount
         self.locktime = locktime
         self.threshold = threshold
@@ -31,6 +50,16 @@ class SECPTransferOutput:
         for address in addresses:
             assert len(address) == 20
 
+    def address_bytes(self):
+        num_addresses = num_to_int(len(self.addresses))
+        return num_addresses + b''.join(self.addresses)
+
+    def to_bytes(self, hex=False):
+        serialized = self.type_id + self.amount + self.locktime + self.threshold + self.address_bytes()
+        if hex:
+            return HexBytes(serialized).hex()
+        return serialized
+
 
 class TransferableOutput:
     def __init__(self, asset_id: bytes, output: SECPTransferOutput):
@@ -38,10 +67,16 @@ class TransferableOutput:
         self.output = output
         assert len(self.asset_id) == 32
 
+    def to_bytes(self, hex=False):
+        serialized = self.asset_id + self.output.to_bytes()
+        if hex:
+            return HexBytes(serialized).hex()
+        return serialized
+
 
 class EVMExportTx:
-    def __init__(self, networkID: bytes, blockchainID: bytes, destinationChain: bytes, # noqa
-                 inputs: list[EVMInput], exportedOutputs: list[TransferableOutput]): # noqa
+    def __init__(self, networkID: bytes, blockchainID: bytes, destinationChain: bytes,  # noqa
+                 inputs: list[EVMInput], exportedOutputs: list[TransferableOutput]):  # noqa
         # typeID for an ExportTx is 1
         self.typeID = num_to_int(1)
         self.networkID = networkID
@@ -53,3 +88,19 @@ class EVMExportTx:
         assert len(self.networkID) == 4
         assert len(self.blockchainID) == 32
         assert len(self.destinationChain) == 32
+
+    def inputs_bytes(self):
+        input_byte_list = [input.to_bytes() for input in self.inputs]
+        num_inputs = num_to_int(len(self.inputs))
+        return num_inputs + b''.join(input_byte_list)
+
+    def outputs_bytes(self):
+        output_byte_list = [output.to_bytes() for output in self.exportedOutputs]
+        num_outputs = num_to_int(len(self.exportedOutputs))
+        return num_outputs + b''.join(output_byte_list)
+
+    def to_bytes(self, hex=False):
+        serialized = self.typeID + self.networkID + self.blockchainID + self.destinationChain + self.inputs_bytes() + self.outputs_bytes()
+        if hex:
+            return HexBytes(serialized).hex()
+        return serialized
