68da94b3df7833e7b445510dc19c02d10c2e58fe
Dalmatina <97528934+ranadalmatina@users.noreply.github.com>
2022-05-22 16:24:16 +0100

    Rudimentary Avalanche RPC client.

--- ss/avalanche/api.py
+++ ss/avalanche/api.py
@@ -1,27 +1,84 @@
 import requests
-RPC_URL = "https://api.avax-test.network/ext/bc/C/avax"
 
 
-def get_atomic_tx(tx_id="2QouvFWUbjuySRxeX5xMbNCuAaKWfbk5FeEa2JmoF85RKLk2dD"):
-    response = requests.post(RPC_URL, json={
-        "jsonrpc":"2.0",
-        "id"     :1,
-        "method" :"avax.getAtomicTxStatus",
-        "params" :{
-            "txID": tx_id
-        }
-    })
-    return response
+class AvalancheGo:
+    """
+    Avalanche Go RPC client.
+    """
+    AVAX_HTTP = "https://api.avax.network"
+    FUJI_HTTP = "https://api.avax-test.network"
+    C_CHAIN = "/ext/bc/C/avax"
+    X_CHAIN = "/ext/bc/X"
+    P_CHAIN = "/ext/bc/P"
+
+    def __init__(self, fuji=True):
+        self.fuji = fuji
+
+    @property
+    def rpc_url(self):
+        return self.FUJI_HTTP if self.fuji else self.AVAX_HTTP
+
+    @property
+    def c_chain_rpc_url(self):
+        return self.rpc_url + self.C_CHAIN
+
+    @property
+    def x_chain_rpc_url(self):
+        return self.rpc_url + self.X_CHAIN
+
+    @property
+    def p_chain_rpc_url(self):
+        return self.rpc_url + self.P_CHAIN
 
 
-def issue_tx(tx: str, encoding="cb58"):
-    response = requests.post(RPC_URL, json={
-        "jsonrpc":"2.0",
-        "id"     : 1,
-        "method" :"avax.issueTx",
-        "params" :{
-            "tx": tx,
-            "encoding": encoding
-        }
-    })
-    return response
+    def evm_get_atomic_tx(self, tx_id):
+        response = requests.post(self.c_chain_rpc_url, json={
+            "jsonrpc":"2.0",
+            "id"     :1,
+            "method" :"avax.getAtomicTxStatus",
+            "params" :{
+                "txID": tx_id
+            }
+        })
+        return response
+
+
+    def evm_issue_tx(self, tx: str, encoding="cb58"):
+        response = requests.post(self.c_chain_rpc_url, json={
+            "jsonrpc":"2.0",
+            "id"     : 1,
+            "method" :"avax.issueTx",
+            "params" :{
+                "tx": tx,
+                "encoding": encoding
+            }
+        })
+        return response
+
+
+    def avm_get_utxos(self, addresses: list[str], source_chain: str, limit=1024, encoding="cb58"):
+        response = requests.post(self.x_chain_rpc_url, json={
+            "jsonrpc":"2.0",
+            "id"     : 1,
+            "method" :"avm.getUTXOs",
+            "params" :{
+                "addresses": addresses,
+                "sourceChain": source_chain,
+                "limit": limit,
+                "encoding": encoding,
+            }
+        })
+        return response
+
+
+    def avm_issue_tx(self, tx: str, encoding="cb58"):
+        response = requests.post(self.x_chain_rpc_url, json={
+            "jsonrpc":"2.0",
+            "id"     : 1,
+            "method" :"avm.issueTx",
+            "params" :{
+                "tx": tx,
+                "encoding": encoding
+            }
+        })
+        return response
