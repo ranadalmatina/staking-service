bb1981655eaf1c3477e9211a0b54035def3dfeaf
Dalmatina <97528934+ranadalmatina@users.noreply.github.com>
2022-05-31 11:55:51 +0100

    By default return ETH style address inside tests.

--- ss/common/testing/address.py
+++ ss/common/testing/address.py
@@ -6,8 +6,7 @@ import hashlib
 def generate_random_address(currency_code: str) -> str:
     randbytes = str(random.random()).encode('utf-8')
 
-    if 'ETH' in currency_code:
-        m = hashlib.sha3_256()
-        m.update(randbytes)
-        hash_ = m.digest()
-        return '0x' + binascii.hexlify(hash_[12:]).decode()
+    m = hashlib.sha3_256()
+    m.update(randbytes)
+    hash_ = m.digest()
+    return '0x' + binascii.hexlify(hash_[12:]).decode()
