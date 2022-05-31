80d9180d2e2f32d8e198a030267cc4ea68afc876
Dalmatina <97528934+ranadalmatina@users.noreply.github.com>
2022-05-31 11:58:57 +0100

    Fix bug in bech32 address function. Write a more generic function that returns the default address type.

--- ss/avalanche/bech32.py
+++ ss/avalanche/bech32.py
@@ -1,5 +1,6 @@
 from bip_utils import Bech32Decoder
 from common.bip import Bip44ConfGetter, Bip44Coins
+from common.bip.address import FujiCChainAddrEncoder
 from bip_utils import Bip44PublicKey, Bip32Secp256k1
 
 
@@ -11,6 +12,25 @@ def bech32_to_bytes(addr: str)-> bytes:
 
 
 def bech32_address_from_public_key(public_key: bytes, coin: Bip44Coins):
+    if coin in (Bip44Coins.FB_C_CHAIN, Bip44Coins.AVAX_C_CHAIN, Bip44Coins.AVAX_C_CHAIN):
+        # The default derivation is Ethereum style address. But we want Bech32
+        bip32_ctx = Bip32Secp256k1.FromPublicKey(public_key)
+        return FujiCChainAddrEncoder.EncodeKey(bip32_ctx.PublicKey().KeyObject())
+
+    bip32_ctx = Bip32Secp256k1.FromPublicKey(public_key)
+    derived_ctx = Bip44PublicKey(bip32_ctx.PublicKey(), Bip44ConfGetter.GetConfig(coin))
+    return derived_ctx.ToAddress()
+
+
+def address_from_public_key(public_key: bytes, coin: Bip44Coins):
+    """
+    Derive address given the public key and the coin type. By default Avalanche C-Chain
+    addresses use the Etheruem style addressing. To get Bech32 style address, use the
+    function above.
+    :param public_key:
+    :param coin:
+    :return:
+    """
     bip32_ctx = Bip32Secp256k1.FromPublicKey(public_key)
     derived_ctx = Bip44PublicKey(bip32_ctx.PublicKey(), Bip44ConfGetter.GetConfig(coin))
     return derived_ctx.ToAddress()
