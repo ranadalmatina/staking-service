d55de263dc8e27db74b16f696691e73174e28688
Azureus <w@rana.ventures>
2022-06-01 15:15:27 -0500

    Restore files

--- ss/avalanche/management/commands/fireblocks_cchain_export_to_pchain.py
+++ ss/avalanche/management/commands/fireblocks_cchain_export_to_pchain.py
@@ -1,7 +1,6 @@
 from hexbytes import HexBytes
 from decimal import Decimal
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from avalanche.base58 import Base58Decoder, Base58Encoder
 from avalanche.tools import num_to_uint32, num_to_uint64
 from avalanche.constants import DEFAULTS
@@ -21,15 +20,14 @@ class Command(BaseCommand):
         self.build_transaction()
 
     def get_nonce(self, address):
-        web3 = AvaWeb3(RPC_URL=settings.AVAX_RPC_URL)
+        web3 = AvaWeb3()
         print(web3.get_balance(address))
         return web3.get_nonce(address)
 
     def get_to_address(self):
         # This is the only derivation path we are allowed on test workspace
         pub_key = fireblocks_public_key("44/1/0/0/0")
-        address = bech32_address_from_public_key(
-            pub_key.ToBytes(), Bip44Coins.FB_P_CHAIN)
+        address = bech32_address_from_public_key(pub_key.ToBytes(), Bip44Coins.FB_P_CHAIN)
         _chain, _bech32 = address.split('-')
         p_address = bech32_to_bytes(_bech32)
         print(HexBytes(p_address).hex())
@@ -38,8 +36,7 @@ class Command(BaseCommand):
     def build_transaction(self):
         pub_key = fireblocks_public_key("44/1/0/0/0")
         from_address = eth_address_from_public_key(pub_key.ToBytes())
-        to_address = bech32_address_from_public_key(
-            pub_key.ToBytes(), Bip44Coins.FB_P_CHAIN)
+        to_address = bech32_address_from_public_key(pub_key.ToBytes(), Bip44Coins.FB_P_CHAIN)
         export_tx = self.build_export_tx(from_address)
         unsigned_tx = UnsignedTransaction(export_tx)
         export_tx = AtomicTx(from_derivation_path="44/1/0/0/0",
@@ -55,17 +52,16 @@ class Command(BaseCommand):
         print('----------HASH----------')
         print(unsigned_tx.hash().hex())
 
+
     def build_export_tx(self, from_address: str):
         network_id = 5
         p_chain_blockchain_id_str: str = DEFAULTS['networks'][network_id]['P']['blockchainID']
         assert p_chain_blockchain_id_str == "11111111111111111111111111111111LpoYY"
-        p_chain_blockchain_id_buf = Base58Decoder.CheckDecode(
-            p_chain_blockchain_id_str)
+        p_chain_blockchain_id_buf = Base58Decoder.CheckDecode(p_chain_blockchain_id_str)
 
         c_chain_blockchain_id_str: str = DEFAULTS['networks'][network_id]['C']['blockchainID']
         assert c_chain_blockchain_id_str == 'yH8D7ThNJkxmtkuv2jgBa4P1Rn3Qpr4pPr7QYNfcdoS6k6HWp'
-        c_chain_blockchain_id_buf = Base58Decoder.CheckDecode(
-            c_chain_blockchain_id_str)
+        c_chain_blockchain_id_buf = Base58Decoder.CheckDecode(c_chain_blockchain_id_str)
 
         avax_asset_id: str = DEFAULTS['networks'][network_id]['X']['avaxAssetID']
         assert avax_asset_id == 'U8iRqJoiJm8xZHAacmvYyZVwqQx6uDNtQeP3CQ6fcgQk3JqnK'
@@ -85,16 +81,13 @@ class Command(BaseCommand):
         network_id = num_to_uint32(5)
 
         print('-----------Inputs / Outputs ---------')
-        in1 = EVMInput(c_address, num_to_uint64(
-            amount + export_fee), avax_asset_id_buf, num_to_uint64(nonce))
+        in1 = EVMInput(c_address, num_to_uint64(amount + export_fee), avax_asset_id_buf, num_to_uint64(nonce))
         print(in1.to_hex())
-        sec_out = SECPTransferOutput(num_to_uint64(
-            amount), locktime, threshold, [p_address])
+        sec_out = SECPTransferOutput(num_to_uint64(amount), locktime, threshold, [p_address])
         print(sec_out.to_hex())
         xfer_out = TransferableOutput(avax_asset_id_buf, sec_out)
         print(xfer_out.to_hex())
-        export_tx = EVMExportTx(
-            network_id, c_chain_blockchain_id_buf, p_chain_blockchain_id_buf, [in1], [xfer_out])
+        export_tx = EVMExportTx(network_id, c_chain_blockchain_id_buf, p_chain_blockchain_id_buf, [in1], [xfer_out])
         print('--------------------------')
         print(export_tx.to_hex())
         return export_tx
