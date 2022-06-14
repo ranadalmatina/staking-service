5ea4127ad4ad82c1e5d51932980a9108a0b06c3f
Dalmatina <97528934+ranadalmatina@users.noreply.github.com>
2022-06-14 20:31:15 +0100

    Use source chain to create ChainSwap model.

--- ss/avalanche/management/commands/chain_swap_c_to_p.py
+++ ss/avalanche/management/commands/chain_swap_c_to_p.py
@@ -1,19 +1,14 @@
 from decimal import Decimal
 
 from django.core.management.base import BaseCommand
-from django.db import transaction
 
-from avalanche.models import ChainSwap
-from avalanche.utils.cchain_export_to_pchain import CChainExportToPChain
+from avalanche.utils.chain_swap import create_chain_swap
 
 
 class Command(BaseCommand):
-    help = "Create a chain swap from C-Chain to P-Chain."
+    help = "Create a chain swap from C-Chain to P-Chain or vice-versa."
 
     def handle(self, *args, **options):
         amount = Decimal("1")  # 1 AVAX
-        with transaction.atomic():
-            export = CChainExportToPChain(network_id=5)
-            export_tx = export.build_transaction(amount=amount)
-            swap = ChainSwap(export_tx=export_tx)
-            swap.save()
+        create_swap_fn = create_chain_swap(source_chain='P')
+        create_swap_fn(amount=amount)
